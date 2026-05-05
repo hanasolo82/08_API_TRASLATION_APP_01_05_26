7c4066347fb94ee262dd42f2ca09f5abc1776d7e
Hanasolo <hanasolo@hotmail.com>
2026-05-05 14:05:45 +0200

    change to second page js

--- public/src/main.js
+++ public/src/main.js
@@ -2,14 +2,13 @@
 const form = document.querySelector("form")
 const button = document.getElementById("form-btn")
 const input = document.querySelector("input")
+const mainEl = document.getElementById("main-container")
+
+form.addEventListener("submit", renderPage)
+form.addEventListener("submit", formData)
 
 
-
-
-
-form.addEventListener("submit", traslateBtnHandler)
-
-function traslateBtnHandler(e) {
+function formData(e) {
     e.preventDefault()
     const formData = new FormData(form)
     const inputName = formData.get(input.name)
@@ -17,6 +16,28 @@ function traslateBtnHandler(e) {
     console.log(textArea)
     console.log(inputName)
     
-    
-    
 }
+
+
+function renderPage(e) {
+    e.preventDefault()
+
+
+    mainEl.innerHTML = `
+                <form>
+                    <fieldset>
+                        <label for="to-translate" class="blue">Orinal text 👇</label>
+                        <textarea id="to-translate" placeholder="How are you?"></textarea>
+                        <label for="translated"  class="blue">Your translation 👇</label>
+                        <textarea id="translated" placeholder=""></textarea>
+                        <button type="submit" class="form-btn" id="form-btn">Star Over</button>
+                    </fieldset>
+                </form>`
+}
+//next steps:
+
+//  on click 
+//  1. change to second page js
+//  2. configure secure ids with server. framework express
+//  3. Api openai configure 
+//  4. Get data and deploy 
