261d2821ede2a918680252d4caae8a96e876bf5c
Hanasolo <hanasolo@hotmail.com>
2026-05-05 13:06:05 +0200

    form data handler function

--- public/src/main.js
+++ public/src/main.js
@@ -1,11 +1,22 @@
 
 const form = document.querySelector("form")
+const button = document.getElementById("form-btn")
+const input = document.querySelector("input")
+
+
+
+
 
 form.addEventListener("submit", traslateBtnHandler)
 
-
-
 function traslateBtnHandler(e) {
     e.preventDefault()
-    console.log("click")
+    const formData = new FormData(form)
+    const inputName = formData.get(input.name)
+    const textArea = document.getElementById("to-translate").value
+    console.log(textArea)
+    console.log(inputName)
+    
+    
+    
 }
