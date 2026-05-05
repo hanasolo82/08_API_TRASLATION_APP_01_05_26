b39e0d55bbb53477a78025db3e90f349eb0f0070
Hanasolo <hanasolo@hotmail.com>
2026-05-05 07:20:45 +0200

    main and form listener added

---
+++ public/src/main.js
@@ -0,0 +1,11 @@
+
+const form = document.querySelector("form")
+
+form.addEventListener("submit", traslateBtnHandler)
+
+
+
+function traslateBtnHandler(e) {
+    e.preventDefault()
+    console.log("click")
+}
